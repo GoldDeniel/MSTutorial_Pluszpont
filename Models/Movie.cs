952f2899e26bf11a8d1a6463731dbc4ad7b4074a
GoldDeniel <aranyadhun@gmail.com>
2024-10-14 11:06:58 +0200

    :bug: Fixed Edit and Create page

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -7,8 +7,8 @@ public class Movie
     public int Id { get; set; }
 
 
-    [Range(1, 100)]
-    [DataType(DataType.Currency)]
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
     public string? Title { get; set; }
 
 
