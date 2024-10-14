68429dcc1a1f58b27e8ecd9dc834d7fd8d4244a1
GoldDeniel <aranyadhun@gmail.com>
2024-10-14 09:19:12 +0200

    :heavy_plus_sign: Add new field

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -118,6 +118,7 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies/Create
+        
         public IActionResult Create()
         {
             return View();
@@ -128,7 +129,7 @@ namespace MvcMovie.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price, Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -160,7 +161,7 @@ namespace MvcMovie.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (id != movie.Id)
             {
--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -28,6 +28,7 @@ public static class SeedData
                     Title = "When Harry Met Sally",
                     ReleaseDate = DateTime.Parse("1989-2-12"),
                     Genre = "Romantic Comedy",
+                    Rating = "R",
                     Price = 7.99M
                 },
                 new Movie
@@ -35,6 +36,7 @@ public static class SeedData
                     Title = "Ghostbusters ",
                     ReleaseDate = DateTime.Parse("1984-3-13"),
                     Genre = "Comedy",
+                    Rating = "S",
                     Price = 8.99M
                 },
                 new Movie
@@ -42,6 +44,7 @@ public static class SeedData
                     Title = "Ghostbusters 2",
                     ReleaseDate = DateTime.Parse("1986-2-23"),
                     Genre = "Comedy",
+                    Rating = "S",
                     Price = 9.99M
                 },
                 new Movie
@@ -49,6 +52,7 @@ public static class SeedData
                     Title = "Rio Bravo",
                     ReleaseDate = DateTime.Parse("1959-4-15"),
                     Genre = "Western",
+                    Rating = "Z", 
                     Price = 3.99M
                 }
             );
